880f9429609e86f173686d1fb527962f822773d0
MrCarter <36346842+MrCarter2959@users.noreply.github.com>
2019-02-06 13:24:21 -0500

    Updated GUI_PasswordGen.PS1

--- GUI_PasswordGen.ps1
+++ GUI_PasswordGen.ps1
@@ -42,7 +42,21 @@ $button1.Location = New-Object Drawing.Point 100,120
 # Set up event handler to extarct text from TextBox and display it on the Label.
 $button.add_click({
 
-Import-Module RandomPasswordEmbedded
+    function RandomPasswordEmbedded() {
+        $Random_InputRange = 100..999
+        
+        $Random_RandomWord = "Actor","Agent","Alarm","Angle","Award","Basic","Began","Block","Brain","Brand","Broad","Broke","Build","Cable","Carry","Chart","Claim","Class","Click","Clock","Delay","Depth","Dozen","Drawn","Dream","Dress","Drive","Drove","Empty","Enjoy","Event","Field","Fixed","Found","Frame","Fresh","Front","Fruit","Great","Heart","Heavy","Horse","Hotel","House","Ideal","Image","Index","Judge","Known","Label","Large","Limit","Links","Logic","Lunch","Magic","Major","Metal","Model","Money","Plate","Power","Proud","Prove","Quick","Quiet","Reach","Ready","River","Royal","Rural","Score","Shape","Share","Sharp","Sheet","Shelf","Smile","Sound","Spare","Speak","Speed","Spend","Table","Thing","Think","Third","Throw","Times","Tired","Title","Topic","Total","Touch","Trade","Trust","Twice","Watch","Wheel","World","Years","Young","Yummy"
+        
+        $Random_Exclude = 69,420,111,222,333,444,555,666,777,888,999,169,269,369,469,569,669,769,869,969,00,11,22,33,44,55,66,77,88,99,100,200,300,400,500,600,700,800,900
+        
+        $Random_RandomNumber = $Random_InputRange | Where-Object { $Random_Exclude -notcontains $_}
+        
+        $Random_RandPassword = Get-Random -InputObject $Random_RandomWord
+        
+        $Random_RandPassword += Get-Random -InputObject $Random_RandomNumber
+        
+        $RandomPasswordEmbedded
+        }
 $Password = RandomPasswordEmbedded
 
 $textfield.Text = $Password
